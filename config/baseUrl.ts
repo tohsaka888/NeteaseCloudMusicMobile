8461207627ee4cc4c4ebd77d7e2ded120ace7c13
tohsaka888 <156132264@qq.com>
2022-01-14 17:28:09 +0800

    请求主路由

---
+++ config/baseUrl.ts
@@ -0,0 +1,4 @@
+// const dev = process.env.NODE_ENV !== "development";
+
+// export const server = dev ? 'http://localhost:3000/' : ''
+export const baseUrl = "https://netease-cloud-music-api-tohsaka888.vercel.app";
