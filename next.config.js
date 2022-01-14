a5296f70bcfc90b7391b4ad44cb1d244bd3e7525
tohsaka888 <156132264@qq.com>
2022-01-14 17:25:59 +0800

    添加图片域名

--- next.config.js
+++ next.config.js
@@ -4,3 +4,19 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+const withImages = require('next-images')
+
+const withTM = require('next-transpile-modules')([
+  'antd-mobile',
+]);
+
+module.exports = withTM(withImages({
+  images: {
+    domains: ['p1.music.126.net', 'p2.music.126.net', 'p3.music.126.net', 'p4.music.126.net'],
+  },
+  experimental: {
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  }
+}));
